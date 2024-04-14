e07b0797fb959d4b9743bd0b1dd1e78dd5f3fab2
Aimen Guedhami <aymenguedhami180@gmail.com>
2024-04-14 21:07:38 +0100

    Added The login component and also added the styling for the login

---
+++ backend/Model/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const User = new mongoose.Schema({
+    Username: { type: String, require: true },
+    Email: { type: String, require: true ,unique:true},
+    Password: { type: String, require: true },
+    Role: { type: String, require: true },
+},
+    { collection: "User" }
+)
+
+const Model = mongoose.model("User",User);
+module.exports = Model;
+
---
+++ src/Components/Login.js
@@ -0,0 +1,74 @@
+import './Login.css';
+import 'boosted/dist/css/boosted.min.css';
+import React, { useState } from 'react';
+
+function Login() {
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+  const [loginStatus, setLoginStatus] = useState(null);
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    try {
+      const response = await fetch('http://localhost:4000/Login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          Email,
+          Password,
+        }),
+      });
+      const data = await response.json();
+      console.log(data);
+      if (data.status === "ok") {
+        localStorage.setItem('token', data.User);
+        setLoginStatus('success');
+        // Navigate to The Other Page
+      } else {
+        setLoginStatus('error');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setLoginStatus('error');
+    }
+  }
+
+  return (
+    <div className="LoginContainer">
+      {loginStatus === 'success' && (
+        <div className="alert alert-success" role="alert">
+          <span className="alert-icon"><span className="visually-hidden">Success</span></span>
+          <p>Log In Successfull Welcome</p>
+        </div>
+      )}
+      {loginStatus === 'error' && (
+        <div className="alert alert-danger" role="alert">
+          <span className="alert-icon"><span className="visually-hidden">Error</span></span>
+          <p>Error Occured While Trying To Log In</p>
+        </div>
+      )}
+      <div className="mb-3 row">
+        <div className="col-sm-12">
+          <label id='labelemail' className="col-form-label">Email :</label>
+          <input type="email" className="form-control input-box" value={Email} onChange={(e) => setEmail(e.target.value)} id="inputEmail" required/>
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <div className="col-sm-12">
+          <label id='labelpassword' className="col-form-label">Password :</label>
+          <input type="password" className="form-control input-box" value={Password} onChange={(e) => setPassword(e.target.value)} id="inputPassword" required/>
+        </div>
+      </div>
+      <div className="mb-3 row">
+        <div className="col-sm-12">
+          <button className="btn btn-primary" type="button" onClick={handleLogin}>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+  
+}
+
+export default Login;
